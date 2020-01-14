5770b57c80b49d63f1c51c201cf797c4e6e2677d
Kelvin Rodriguez <krodriguez@usgs.gov>
2020-01-13 21:15:14 -0700

    added clustered cim

--- autocnet/matcher/cross_instrument_matcher.py
+++ autocnet/matcher/cross_instrument_matcher.py
@@ -42,8 +42,12 @@ from shapely import wkt
 from shapely.geometry.multipolygon import MultiPolygon
 from shapely.geometry import Point
 
+from redis import StrictRedis
+
+from plurmy import Slurm
+
 from autocnet import config, engine, Session
-from autocnet.io.db.model import Images, Points, Measures
+from autocnet.io.db.model import Images, Points, Measures, JsonEncoder
 from autocnet.graph.network import NetworkCandidateGraph
 from autocnet.matcher.subpixel import iterative_phase
 from autocnet.cg.cg import distribute_points_in_geom
@@ -109,10 +113,9 @@ def generate_ground_points(ground_db_config, nspts_func=lambda x: int(round(x,1)
 
     # just hard code queries to the mars database as it exists for now
 
-    ground_image_query = f'select * from themisdayir where ST_INTERSECTS(geom, ST_MakeEnvelope({image_fp_bounds[0]}, {image_fp_bounds[1]}, {image_fp_bounds[2]}, {image_fp_bounds[3]}, {config["spatial"]["latitudinal_srid"]}))'
+    ground_image_query = f'select * from themisdayir where ST_INTERSECTS(geom, ST_MakeEnvelope({fp_poly_bounds[0]}, {fp_poly_bounds[1]}, {fp_poly_bounds[2]}, {fp_poly_bounds[3]}, {config["spatial"]["latitudinal_srid"]}))'
     themis_images = gpd.GeoDataFrame.from_postgis(ground_image_query,
                                                   ground_engine, geom_col="geom")
-    spatial_index = themis_images.sindex
 
     coords = distribute_points_in_geom(fp_poly, nspts_func=nspts_func, ewpts_func=ewpts_func)
     coords = np.asarray(coords)
@@ -167,7 +170,143 @@ def generate_ground_points(ground_db_config, nspts_func=lambda x: int(round(x,1)
         ground_cnet.loc[index, 'resolution'] = resolutions
 
     ground_cnet = gpd.GeoDataFrame(ground_cnet, geometry='point')
-    return ground_cnet, ground_poly, coords
+    return ground_cnet, fp_poly, coords
+
+
+def propagate_point(lon, lat, pointid, paths, lines, samples, resolutions):
+    """
+
+    """
+    images = gpd.GeoDataFrame.from_postgis(f"select * from images where ST_Intersects(geom, ST_SetSRID(ST_Point({lon}, {lat}), {config['spatial']['latitudinal_srid']}))", engine, geom_col="geom")
+
+    image_measures = pd.DataFrame(zip(paths, lines, samples, resolutions), columns=["path", "line", "sample", "resolution"])
+    measure = image_measures.iloc[0]
+
+    p = Point(lon, lat)
+    new_measures = []
+
+    # lazily iterate for now
+    for i,image in images.iterrows():
+        res = isis.point_info(image["path"], p.x, p.y, point_type="ground", allow_outside=False)
+        dest_line, dest_sample = res["GroundPoint"]["Line"], res["GroundPoint"]["Sample"]
+
+        try:
+            dest_resolution = res["GroundPoint"]["LineResolution"].value
+        except:
+            warnings.warn(f'Failed to generate ground point info on image {image["path"]} at lat={p.y} lon={p.x}')
+            continue
+
+        dest_data = GeoDataset(image["path"])
+        dest_arr = dest_data.read_array()
+
+        # list of matching results in the format:
+        # [measure_index, x_offset, y_offset, offset_magnitude]
+        match_results = []
+        for k,m in image_measures.iterrows():
+            # dynamically set scale based on point resolution
+            dest_to_base_scale = dest_resolution/m["resolution"]
+
+            scaled_dest_line = (dest_arr.shape[0]-dest_line)*dest_to_base_scale
+            scaled_dest_sample = dest_sample*dest_to_base_scale
+
+            scaled_dest_arr = imresize(dest_arr, dest_to_base_scale)[::-1]
+
+            base_arr = GeoDataset(m["path"]).read_array()
+
+            sx, sy = m["sample"], m["line"]
+            dx, dy = scaled_dest_sample, scaled_dest_line
+            try:
+                # not sure what the best parameters are here
+                ret = iterative_phase(sx, sy, dx, dy, base_arr, scaled_dest_arr, size=10, reduction=1, max_dist=2, convergence_threshold=1)
+            except Exception as ex:
+                match_results.append(ex)
+                continue
+
+            if ret is not None and None not in ret:
+                x,y,metrics = ret
+            else:
+                match_results.append("Failed to Converge")
+                continue
+
+            dist = np.linalg.norm([x-dx, -1*(y-dy)])
+            match_results.append([k, x-dx, -1*(y-dy), dist])
+
+        # get best offsets, if possible we need better metric for what a
+        # good match looks like
+        match_results = np.asarray([res for res in match_results if isinstance(res, list)])
+        if match_results.shape[0] == 0:
+            # no matches
+            print("No Mathces")
+            continue
+
+        match_results = match_results[np.argwhere(match_results[:,3] == match_results[:,3].min())][0][0]
+
+        if match_results[3] > 2:
+            # best match drifted too much
+            continue
+
+        # apply offsets
+        sample = (match_results[1]/dest_to_base_scale) + dest_sample
+        line = (match_results[2]/dest_to_base_scale) + dest_line
+
+        pointpvl = isis.point_info(image["path"], sample, line, point_type="image")
+        groundx, groundy, groundz = pointpvl["GroundPoint"]["BodyFixedCoordinate"].value
+        groundx, groundy, groundz = groundx*1000, groundy*1000, groundz*1000
+
+        new_measures.append({
+                'pointid' : pointid,
+                'imageid' : image['id'],
+                'serial' : image['serial'],
+                'line' : line,
+                'sample' : sample,
+                'point_latlon' : p,
+                'point_ground' : Point(groundx, groundy, groundz)
+        })
+
+    return new_measures
+
+def cluster_propagate_control_network(base_cnet, walltime='00:20:00', chunksize=1000, exclude=None):
+    warnings.warn('This function is not well tested. No tests currently exists \
+    in the test suite for this version of the function.')
+
+    # Setup the redis queue
+    rqueue = StrictRedis(host=config['redis']['host'],
+                         port=config['redis']['port'],
+                         db=0)
+
+    # Push the job messages onto the queue
+    queuename = config['redis']['processing_queue']
+
+    groups = base_cnet.groupby('pointid').groups
+    for cpoint, indices in groups.items():
+        measures = base_cnet.loc[indices]
+        measure = measures.iloc[0]
+
+        p = measure.point
+
+        # get image in the destination that overlap
+        lon, lat = measures["point"].iloc[0].xy
+        msg = {'lon' : lon[0],
+               'lat' : lat[0],
+               'cpoint' : cpoint,
+               'paths' : measures['path'].tolist(),
+               'lines' : measures['line'].tolist(),
+               'samples' : measures['sample'].tolist(),
+               'resolutions' : measures['resolution'].tolist(),
+               'walltime' : walltime}
+        rqueue.rpush(queuename, json.dumps(msg, cls=JsonEncoder))
+
+    # Submit the jobs
+    submitter = Slurm('acn_propagate',
+                 job_name='cross_instrument_matcher',
+                 mem_per_cpu=config['cluster']['processing_memory'],
+                 time=walltime,
+                 partition=config['cluster']['queue'],
+                 output=config['cluster']['cluster_log_dir']+'/autocnet.cim-%j')
+    job_counter = len(groups.items())
+    submitter.submit(array='1-{}'.format(job_counter))
+    return job_counter
+
 
 
 def propagate_control_network(base_cnet):
@@ -176,113 +315,23 @@ def propagate_control_network(base_cnet):
     """
     warnings.warn('This function is not well tested. No tests currently exists \
     in the test suite for this version of the function.')
-    dest_images = gpd.GeoDataFrame.from_postgis("select * from images", engine, geom_col="geom")
-    spatial_index = dest_images.sindex
-    groups = base_cnet.groupby('pointid').groups
 
-    # append to list if images, mostly used for working with the network in python
-    # after this step, is this uncecceary outside of debugging? Maybe actually should return
-    # more info of where everything was sourced in the original DataFrames?
-    images = []
+    groups = base_cnet.groupby('pointid').groups
 
     # append CNET info into structured Python list
     constrained_net = []
-    dbpoints = []
-    dbmeasures = []
-    i = 0
-    ngroups = len(groups)
+
     # easily parrallelized on the cpoint level, dummy serial for now
     for cpoint, indices in groups.items():
-        i+=1
-        print(f"{i}/{ngroups}")
         measures = base_cnet.loc[indices]
         measure = measures.iloc[0]
 
         p = measure.point
 
         # get image in the destination that overlap
-        matches = dest_images[dest_images.intersects(p)]
-
-        # lazily iterate for now
-        for i,row in matches.iterrows():
-            res = isis.point_info(row["path"], p.x, p.y, point_type="ground", allow_outside=False)
-            dest_line, dest_sample = res["GroundPoint"]["Line"], res["GroundPoint"]["Sample"]
-
-            try:
-                dest_resolution = res["GroundPoint"]["LineResolution"].value
-            except:
-                warnings.warn(f'Failed to generate ground point info on image {row["path"]} at lat={p.y} lon={p.x}')
-                continue
-
-            dest_data = GeoDataset(row["path"])
-            dest_arr = dest_data.read_array()
-
-            # dynamically set scale based on point resolution
-            dest_to_base_scale = dest_resolution/measure["resolution"]
-
-            scaled_dest_line = (dest_arr.shape[0]-dest_line)*dest_to_base_scale
-            scaled_dest_sample = dest_sample*dest_to_base_scale
-
-            dest_arr = imresize(dest_arr, dest_to_base_scale)[::-1]
-
-            # list of matching results in the format:
-            # [measure_index, x_offset, y_offset, offset_magnitude]
-            match_results = []
-            for k,m in measures.iterrows():
-                base_arr = GeoDataset(m["path"]).read_array()
-
-                sx, sy = m["sample"], m["line"]
-                dx, dy = scaled_dest_sample, scaled_dest_line
-                try:
-                    # not sure what the best parameters are here
-                    ret = iterative_phase(sx, sy, dx, dy, base_arr, dest_arr, size=10, reduction=1, max_dist=2, convergence_threshold=1)
-                except Exception as ex:
-                    match_results.append(ex)
-                    continue
-
-                if ret is not None and None not in ret:
-                    x,y,metrics = ret
-                else:
-                    match_results.append("Failed to Converge")
-                    continue
-
-                dist = np.linalg.norm([x-dx, -1*(y-dy)])
-                match_results.append([k, x-dx, -1*(y-dy), dist])
-
-            # get best offsets, if possible we need better metric for what a
-            # good match looks like
-            match_results = np.asarray([res for res in match_results if isinstance(res, list)])
-            if match_results.shape[0] == 0:
-                # no matches
-                print("No Mathces")
-                continue
-
-            match_results = match_results[np.argwhere(match_results[:,3] == match_results[:,3].min())][0][0]
-
-            if match_results[3] > 2:
-                # best match diverged too much
-                continue
-
-            measure = measures.loc[match_results[0]]
-
-            # apply offsets
-            sample = (match_results[1]/dest_to_base_scale) + dest_sample
-            line = (match_results[2]/dest_to_base_scale) + dest_line
-
-            pointpvl = isis.point_info(row["path"], sample, line, point_type="image")
-            groundx, groundy, groundz = pointpvl["GroundPoint"]["BodyFixedCoordinate"].value
-            groundx, groundy, groundz = groundx*1000, groundy*1000, groundz*1000
-
-            images.append(row["path"])
-            constrained_net.append({
-                    'pointid' : cpoint,
-                    'imageid' : row['id'],
-                    'serial' : row.serial,
-                    'line' : line,
-                    'sample' : sample,
-                    'point_latlon' : p,
-                    'point_ground' : Point(groundx, groundy, groundz)
-                })
+        lon, lat = measures["point"].iloc[0].xy
+        gp_measures = propagate_point(lon[0], lat[0], cpoint, measures["path"], measures["line"], measures["sample"], measures["resolution"])
+        constrained_net.extend(gp_measures)
 
     ground = gpd.GeoDataFrame.from_dict(constrained_net).set_geometry('point_latlon')
     groundpoints = ground.groupby('pointid').groups
