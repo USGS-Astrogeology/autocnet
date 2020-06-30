db18aace411561a8801132f83a1bcd66803ed978
Kelvin Rodriguez <krodriguez@usgs.gov>
2020-06-30 13:24:11 -0700

    added ok and okm methods

--- autocnet/cg/change_detection.py
+++ autocnet/cg/change_detection.py
@@ -1,5 +1,8 @@
 import numpy as np
 from matplotlib.path import Path
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 from shapely.geometry import Point, MultiPoint
 import geopandas as gpd
 
@@ -21,7 +24,111 @@ def image_diff(arr1, arr2):
      return diff
 
 
-def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1}, cluster_params={"min_samples": 10, "max_eps": 10, "eps": .5, "xi":.5},  image_func=image_diff):
+def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", image_func=image_diff,
+                      extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1}):
+     """
+     Simple change detection algorithm which produces an overlay image of change hotspots
+     (i.e. a 2d histogram image of detected change density).
+
+     Largely based on a method created by Chris Okubo and Brendon Bogar. Histogram step
+     was added for readability.
+
+
+
+     image1
+           \
+             image subtraction/ratio -> feature_extraction -> feature_histogram
+           /
+     image2
+
+     TODO: Paper/abstract might exist, cite
+
+     Parameters
+     ----------
+
+     image1 : GeoDataset
+             Image representing the "before" state of the ROI
+
+     image2 : GeoDataset
+             Image representing the "after" state of the ROI
+
+     image_func : callable
+              Function use to create a derived image from image1 and image2, which in turn is
+              the input for the feature extractor. Default function create a difference image.
+
+     nbins : int
+            number of bins to use in the 2d histogram
+
+     extractor_method : {'orb', 'sift', 'fast', 'surf', 'vl_sift'}
+               The detector method to be used.  Note that vl_sift requires that
+               vlfeat and cyvlfeat dependencies be installed.
+
+     extractor_kwargs : dict
+                        A dictionary containing OpenCV SIFT parameters names and values.
+
+     See Also
+     --------
+
+     feature extractor: autocnet.matcher.cpu_extractor.extract_features
+
+     """
+
+     arr1 = image1.read_array()
+     arr2 = image2.read_array()
+     arr1[arr1 == arr1.min()] = 0
+     arr2[arr2 == arr2.min()] = 0
+     arr1 = bytescale(arr1)
+     arr2 = bytescale(arr2)
+
+     bdiff = image_func(arr1, arr2)
+
+     keys, descriptors = extract_features(bdiff, extractor_method, extractor_parameters=extractor_kwargs)
+     x,y = keys["x"], keys["y"]
+
+     points = [Point(xval, yval) for xval,yval in zip(x,y)]
+
+     heatmap, xedges, yedges = np.histogram2d(y, x, bins=nbins, range=[[0, bdiff.shape[0]], [0, bdiff.shape[1]]])
+     heatmap = cv2.resize(heatmap, dsize=(bdiff.shape[1], bdiff.shape[0]), interpolation=cv2.INTER_NEAREST)
+
+     return points, heatmap, bdiff
+
+
+def obm_detector(image1, image2, nbins=50, extractor_method="orb",  image_func=image_diff,
+                 extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1},
+                 cluster_params={"min_samples": 10, "max_eps": 10, "eps": .5, "xi":.5}):
+     """
+     okobubogar modified detector, experimental feature based change detection algorithmthat expands on okobubogar to allow for
+     programmatic change detection. Returns detected feature changes as weighted polygons.
+
+
+     Parameters
+     ----------
+
+     image1 : GeoDataset
+             Image representing the "before" state of the ROI
+
+     image2 : GeoDataset
+             Image representing the "after" state of the ROI
+
+     image_func : callable
+              Function use to create a derived image from image1 and image2, which in turn is
+              the input for the feature extractor. Default function create a difference image.
+
+     nbins : int
+            number of bins to use in the 2d histogram
+
+     extractor_method : {'orb', 'sift', 'fast', 'surf', 'vl_sift'}
+               The detector method to be used.  Note that vl_sift requires that
+               vlfeat and cyvlfeat dependencies be installed.
+
+     extractor_kwargs : dict
+                        A dictionary containing OpenCV SIFT parameters names and values.
+
+     cluster_params : dict
+                      A dictionary containing sklearn.cluster.OPTICS parameters
+
+     """
+
      arr1 = image1.read_array()
      arr2 = image2.read_array()
      arr1[arr1 == arr1.min()] = 0
@@ -69,4 +176,5 @@ def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", extracto
          weights.append(weight)
 
      return polys, weights, bdiff
+
 
