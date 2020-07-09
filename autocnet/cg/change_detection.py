ae2278d6f7460153f5ac4e6a90907db6b8397f22
Kelvin Rodriguez <krodriguez@usgs.gov>
2020-07-08 19:38:00 -0700

    added applet

--- autocnet/cg/change_detection.py
+++ autocnet/cg/change_detection.py
@@ -8,6 +8,7 @@ from plio.io.io_gdal import GeoDataset
 
 from autocnet.utils.utils import bytescale
 from autocnet.matcher.cpu_extractor import extract_features
+from matplotlib import pyplot as plt
 
 def image_diff(arr1, arr2):
      arr1 = arr1.astype("float32")
@@ -20,6 +21,21 @@ def image_diff(arr1, arr2):
 
      return diff
 
+def image_ratio(arr1, arr2):
+     arr1 = arr1.astype("float32")
+     arr2 = arr2.astype("float32")
+     arr1[arr1 == 0] = np.nan
+     arr2[arr2 == 0] = np.nan
+
+     ratio = arr1/arr2
+     ratio[np.isnan(ratio)] = 0
+
+     return ratio
+
+func_map = {
+    "diff" : image_diff,
+    "ratio" : image_ratio
+}
 
 def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", image_func=image_diff,
                       extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1}):
@@ -30,13 +46,7 @@ def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", image_fu
      Largely based on a method created by Chris Okubo and Brendon Bogar. Histogram step
      was added for readability.
 
-
-
-     image1
-           \
-             image subtraction/ratio -> feature_extraction -> feature_histogram
-           /
-     image2
+     image1, image2 -> image subtraction/ratio -> feature extraction -> feature histogram
 
      TODO: Paper/abstract might exist, cite
 
@@ -92,6 +102,12 @@ def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", image_fu
      if isinstance(image2, GeoDataset):
          image2 = image2.read_array()
 
+     if isinstance(image_func, str):
+         try:
+             image_func = func_map[image_func]
+         except Exception as e:
+             raise Exception(f"{image_func} is not a valid method, available image functions: {func_map.keys()}")
+
      image1[image1 == image1.min()] = 0
      image2[image2 == image2.min()] = 0
      arr1 = bytescale(image1)
@@ -110,9 +126,9 @@ def okubogar_detector(image1, image2, nbins=50, extractor_method="orb", image_fu
      return points, heatmap, bdiff
 
 
-def okbm_detector(image1, image2, nbins=50, extractor_method="orb",  image_func=image_diff,
+def okbm_detector(image1, image2, extractor_method="orb",  image_func=image_diff,
                  extractor_kwargs={"nfeatures": 2000, "scaleFactor": 1.1, "nlevels": 1},
-                 cluster_params={"min_samples": 10, "max_eps": 10, "eps": .5, "xi":.5}):
+                 cluster_kwargs={"min_samples": 10, "max_eps": 10, "eps": .5, "xi":.5}):
      """
      okobubogar modified detector, experimental feature based change detection algorithmthat expands on okobubogar to allow for
      programmatic change detection. Returns detected feature changes as weighted polygons.
@@ -148,9 +164,6 @@ def okbm_detector(image1, image2, nbins=50, extractor_method="orb",  image_func=
                   >>> image1, image2 = random.random((50,50)), random.random((50,50))
                   >>> results = okbm_detector(image1, image2, image_func=lambda im1, im2: im1/im2)
 
-     nbins : int
-            number of bins to use in the 2d histogram
-
      extractor_method : {'orb', 'sift', 'fast', 'surf', 'vl_sift'}
                The detector method to be used.  Note that vl_sift requires that
                vlfeat and cyvlfeat dependencies be installed.
@@ -158,30 +171,13 @@ def okbm_detector(image1, image2, nbins=50, extractor_method="orb",  image_func=
      extractor_kwargs : dict
                         A dictionary containing OpenCV SIFT parameters names and values.
 
-     cluster_params : dict
+     cluster_kwargs : dict
                       A dictionary containing sklearn.cluster.OPTICS parameters
 
      """
+     points, _, bdiff = okubogar_detector(image1, image2, extractor_method, image_func, extractor_kwargs)
 
-     if isinstance(image1, GeoDataset):
-         image1 = image1.read_array()
-
-     if isinstance(image2, GeoDataset):
-         image2 = image2.read_array()
-
-     image1[image1 == image1.min()] = 0
-     image2[image2 == image2.min()] = 0
-     arr1 = bytescale(image1)
-     arr2 = bytescale(image2)
-
-     bdiff = image_func(arr1, arr2)
-
-     keys, descriptors = extract_features(bdiff, extractor_method, extractor_parameters=extractor_kwargs)
-     x,y = keys["x"], keys["y"]
-
-     points = [Point(xval, yval) for xval,yval in zip(x,y)]
-
-     optics = OPTICS(**cluster_params).fit(list(zip(x,y)))
+     optics = OPTICS(**cluster_kwargs).fit(list(zip(x,y)))
 
      classes = gpd.GeoDataFrame(columns=["label", "point"], geometry="point")
      classes["label"] = optics.labels_
