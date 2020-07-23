aba4ce4527b4a4fe92739aebd7b3bed4d2c48e5e
Kelvin <kelvinrr@icloud.com>
2020-07-23 13:43:13 -0700

    no deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 travis-sphinx -v deploy -b dev
 source deactivate
 conda install conda-build anaconda-client
-conda config --set anaconda_upload yes
+conda config --set anaconda_upload no
 conda build --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
