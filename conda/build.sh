32996b71cf30e293cfabf6a4749a32da70abaead
Kelvin <kelvinrr@icloud.com>
2020-07-24 00:28:23 -0700

    prefix

--- conda/build.sh
+++ conda/build.sh
@@ -5,4 +5,4 @@ export LD_LIBRARY_PATH=/usr/local/lib:/lib:/usr/lib
 
 pip install https://files.pythonhosted.org/packages/a7/7c/24fb0511df653cf1a5d938d8f5d19802a88cef255706fdda242ff97e91b7/redis-3.5.3-py2.py3-none-any.whl
 
-pip install . --no-deps -vv
+pip install . --no-deps -vv --prefix=$PREFIX
