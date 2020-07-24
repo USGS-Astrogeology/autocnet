bad02b4b3b1254626236d5207e68290ddbed564f
Kelvin <kelvinrr@icloud.com>
2020-07-23 21:59:56 -0700

    explicit install

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ conda install conda-build anaconda-client
 conda config --set anaconda_upload no
 conda config --set channel_priority strict
 
+pip install https://files.pythonhosted.org/packages/b1/66/0acc61f28ba6081da470120a503cc8855da8bab35d355ce037463486d362/travis_wait_improved-1.0.0-py3-none-any.whl
+
 travis-wait-improved --timeout 30m conda build --python $PYTHON_VERSION -c conda-forge -c usgs-astrogeology conda
