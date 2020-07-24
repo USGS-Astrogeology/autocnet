f95040378fb004a6c26194a136422192786c28c9
Kelvin <kelvinrr@icloud.com>
2020-07-23 20:48:39 -0700

    cleaned up meta

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-export LD_LIBRARY_PATH=$(HOME)/lib:/usr/local/lib:/lib:/usr/lib
+export LD_LIBRARY_PATH=/usr/local/lib:/lib:/usr/lib
 
 pip install https://files.pythonhosted.org/packages/a7/7c/24fb0511df653cf1a5d938d8f5d19802a88cef255706fdda242ff97e91b7/redis-3.5.3-py2.py3-none-any.whl
 
