54dbdb854dcc07bd07c2be5fb59d5963421a5e79
Kelvin <kelvinrr@icloud.com>
2020-07-23 21:44:37 -0700

    strict channel order

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ travis-sphinx -v deploy -b dev
 source deactivate
 conda install conda-build anaconda-client
 conda config --set anaconda_upload no
-conda build --python $PYTHON_VERSION -c conda-forge -c menpo -c usgs-astrogeology conda
+conda config --set channel_priority strict
+conda build --python $PYTHON_VERSION -c conda-forge -c usgs-astrogeology conda
