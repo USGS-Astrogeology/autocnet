103356629073038423f262cf7c87b0a66ed6b6ca
Kelvin <kelvinrr@icloud.com>
2020-07-23 21:47:44 -0700

    wait improved

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ source deactivate
 conda install conda-build anaconda-client
 conda config --set anaconda_upload no
 conda config --set channel_priority strict
-conda build --python $PYTHON_VERSION -c conda-forge -c usgs-astrogeology conda
+
+travis-wait-improved --timeout 30m conda build --python $PYTHON_VERSION -c conda-forge -c usgs-astrogeology conda
